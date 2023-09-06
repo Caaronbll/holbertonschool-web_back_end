13ce164c6835b38878879b8b5706d057d9718357
Caaronbll <aaronbll46@gmail.com>
2023-09-06 21:14:27 +0000

    changing from var to let

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
-    var new_task = true;
-    var new_task2 = false;
+    let new_task = true;
+    let new_task2 = false;
   }
 
   return [task, task2];
