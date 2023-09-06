2feece2517aaca9c494a3d4e9b6c936854599e77
Caaronbll <aaronbll46@gmail.com>
2023-09-06 21:16:29 +0000

    changing let to const

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    let new_task = true;
-    let new_task2 = false;
+    const new_task = true;
+    const new_task2 = false;
   }
 
   return [task, task2];
