3ffa2c83da2d57b63a696b0f34d2bf25142878ad
Caaronbll <aaronbll46@gmail.com>
2023-09-07 00:33:29 +0000

    6 and 8

--- ES6_basic/8-getBudgetCurrentYear.js
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -3,11 +3,11 @@ function getCurrentYear() {
   return date.getFullYear();
 }
 
-export default function getBudgetForCurrentYear(income = `income-${getCurrentYear()}`, gdp = `gdp-${getCurrentYear()}`, capita = `capita-${getCurrentYear()}`) {
+export default function getBudgetForCurrentYear(income, gdp, capita) {
   const budget = {
-    income,
-    gdp,
-    capita
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita,
   };
 
   return budget;
