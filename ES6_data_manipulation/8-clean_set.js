8549f3c5cb2dd8c58a7652fa155be2b783ba1d6e
Caaronbll <aaronbll46@gmail.com>
2023-09-11 22:24:05 +0000

    task 8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,9 @@
 export default function cleanSet(set, startString) {
-  const filteredSet = Array.from(set).filter((item) => item.startsWith(startString))
-  .map(value => value.slice(startString.length));
-  return filteredSet.join('-');
+  if (startString.length == '') {
+    return '';
+  } else {
+    const filteredSet = Array.from(set).filter((item) => item.startsWith(startString))
+    .map(value => value.slice(startString.length));
+    return filteredSet.join('-');
+  }
 }
