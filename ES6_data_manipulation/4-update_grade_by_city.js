57c016efdb1374629ee92d38f9e0e92dd0a4736d
Caaronbll <aaronbll46@gmail.com>
2023-09-11 17:48:15 +0000

    linter

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,10 +1,10 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
   const updatedStudents = students.filter((student) => student.location === city);
   return updatedStudents.map((student) => {
-  const filteredGrades = newGrades.filter((grade) => grade.studentId === student.id);
-  if (filteredGrades.length > 0) {
-    return {...student, grade: filteredGrades[0].grade, };
-  }
-    return {...student, grade: 'N/A',};
+    const filteredGrades = newGrades.filter((grade) => grade.studentId === student.id);
+    if (filteredGrades.length > 0) {
+      return {...student, grade: filteredGrades[0].grade, };
+    }
+      return {...student, grade: 'N/A',};
   });
 }
