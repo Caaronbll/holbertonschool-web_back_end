8c2427b3ae97963b2bd653bcdce323eebaea40b1
Caaronbll <aaronbll46@gmail.com>
2023-09-11 17:41:45 +0000

    task 4

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,12 @@
+export default function updateStudentGradeByCity(students, city, newGrades='N/A') {
+  const updatedStudents = students.filter((student) => {student.location === city});
+  return updatedStudents.map((student ) => {
+  const filteredGrades = newGrades.filter((grade) => {grade.studentId === student.id});
+  if (filteredGrades.length > 0) {
+    return {...student, grade: filteredGrades[0].grade};
+  }
+  if (filteredGrades.length === 0) {
+    return {...student, grade: 'N/A'};
+  };
+  });
+}
