390178899078ede382254eaa6dbd4bc4202e9d5b
Caaronbll <aaronbll46@gmail.com>
2023-09-12 19:01:05 +0000

    added const

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -2,7 +2,7 @@ export default function updateUniqueItems(map) {
   if (typeof(map) !== Map) {
     throw new Error('Cannot process');
   }
-  for (key in map) {
+  for (const key in map) {
     if (map[key] === 1) {
       map.update(key === 100);
     }
