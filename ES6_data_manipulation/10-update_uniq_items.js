6658e42d0f3f069bd623c5e61564bf744be490e9
Caaronbll <aaronbll46@gmail.com>
2023-09-12 18:47:03 +0000

    testing 10

---
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,11 @@
+export default function updateUniqueItems(map) {
+  if (typeof(map) == Map) {
+    for (map[key] in map) {
+        if (map[key] == 1) {
+            map[key] = 100;
+        }
+    }
+  } else {
+    throw new Error('Cannot process');
+  }
+}
