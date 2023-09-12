060003cbd44249baf6466ea83562033fa681d4fd
Caaronbll <aaronbll46@gmail.com>
2023-09-12 18:01:39 +0000

    adding undefines

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(set, startString) {
   if (startString === '' || typeof startString !== 'string') return '';
-  const filteredSet = Array.from(set).filter((item) => item.startsWith(startString))
+  const filteredSet = Array.from(set).filter((item) => item !== undefined && item.startsWith(startString))
     .map((value) => value.slice(startString.length));
 
   return filteredSet.join('-');
