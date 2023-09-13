0de0b91d88ece11e99e888aed3e8028b85431646
Caaronbll <aaronbll46@gmail.com>
2023-09-13 19:55:21 +0000

    task 3

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,5 +1,7 @@
 import {uploadPhoto, createUser} from "./utils.js";
 
 export default function handleProfileSignup() {
-  
+  return Promise.all([uploadPhoto(), createUser()]).then((data) => {
+    console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`);
+  }).catch(() => console.log('Signup system offline'));
 }
