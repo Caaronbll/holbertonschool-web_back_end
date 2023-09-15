5c29f33b1ecbd6d82d97edc34d7150189bbde834
Caaronbll <aaronbll46@gmail.com>
2023-09-15 15:41:12 +0000

    linter

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -5,14 +5,14 @@ export default async function handleProfileSignup(
   firstName,
   lastName,
   fileName,
-  ) {
-    const status = [];
-    await signUpUser(firstName, lastName)
+) {
+  const status = [];
+  await signUpUser(firstName, lastName)
     .then(async (data) => {
       status.push({ status: 'fulfilled', value: data });
       await uploadPhoto(fileName);
     }).catch((err) => {
-      status.push({ status:'rejected', value: err.toString() });
+      status.push({ status: 'rejected', value: err.toString() });
     });
-    return status;
+  return status;
 }
