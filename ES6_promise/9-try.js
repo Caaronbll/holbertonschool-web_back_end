b4f7f93ffbd52458e2847b2b2f77951ad9bfe406
Caaronbll <aaronbll46@gmail.com>
2023-09-17 05:14:16 +0000

    task 9 linter

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -1,6 +1,8 @@
 export default function guardrail(mathFunction) {
   const queue = [];
-  try { queue.push(mathFunction()) }
+  try {
+    queue.push(mathFunction());
+  }
   catch (e) {
     queue.push(e.toString());
   } finally {
